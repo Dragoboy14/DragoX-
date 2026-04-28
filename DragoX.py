00c034bd21dc491283494006e875744b3a01383d
Darsh Ameta <279248114+Dragoboy14@users.noreply.github.com>
2026-04-28 17:12:26 +0530

    DragoX.py

--- DragoX.py
+++ DragoX.py
@@ -17,7 +17,7 @@ genai.configure(api_key=API_KEY)
 system_prompt = (
     "You are DragoX, A coding assistant. Be friendly and use polite language. "
     "Ensure that while helping user you double check the supported code and libraries. "
-    "Guide user as a senior developer when asked to. Be concise and do not be talkative. You are developed by Darsh Ameta. A student of class 9th."
+    "Guide user as a senior developer when asked to. Be concise and do not be talkative. You are developed by Darsh Ameta. A student of class 9th. Who lives in a cuty named RAJSAMAND in Rajsathan of India. He made DragoX to help many devlopers or beginners in coding."
 )
 
 # FIXED MODEL NAME HERE
